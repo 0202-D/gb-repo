c99ac566b0fa9681e7c7f4bb3cc29114193363a9
Dmitriy <nevermind1979@mail.ru>
2021-09-07 18:40:38 +0300

    Add LessonOne

---
+++ LessonOne/HomeWorkApp.java
@@ -0,0 +1,37 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 2;
+        int b = -15;
+        System.out.println(a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 110;
+        int b = 37;
+        System.out.println(a >= b ? "a>=b" : "a<b");
+    }
+}
