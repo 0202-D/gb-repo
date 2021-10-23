e962437eff56f8e151847e517442b92fd371940c
Dmitriy <nevermind1979@mail.ru>
2021-10-23 13:21:18 +0300

    Update LessonNine

---
+++ LessonNineUpdate/MyChatWindow.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyChatWindow extends JFrame {
+    public MyChatWindow() {
+        setTitle("MyFirstChat");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(600, 200, 500, 500);
+        setLayout(null);
+        JTextField field = new JTextField();
+        field.setBounds(20, 20, 120, 40);
+        add(field);
+        JButton button = new JButton("Send");
+        button.setBounds(20, 60, 120, 32);
+        add(button);
+        JTextArea textArea = new JTextArea();
+        textArea.setBounds(120, 250, 250, 100);
+        add(textArea);
+        JLabel label = new JLabel();
+        label.setText("Sent messages");
+        label.setBounds(120,220,100,20);
+        add(label);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String text = field.getText();
+                if(text !=null){
+                textArea.append(text +"\n");
+                field.setText(null);}
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyChatWindow();
+    }
+}
